789451d9c0b7a363edf1a9a413dc38f60838ef69
natootoki <ikoan0226@gmail.com>
2023-09-29 21:23:01 +0900

    canvas test

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hello, javaScript!")
+
+var canvas = document.getElementById("myCanvas"); // Canvas要素を取得
+var dataURL = canvas.toDataURL(); // Canvasの内容を画像データURLに変換
+
+var image = new Image();
+image.src = dataURL;
+document.body.appendChild(image); // 画像を表示
+
+var link = document.createElement('a');
+link.href = dataURL;
+link.download = 'canvas_image.png'; // 画像のダウンロード時のファイル名
+link.click(); // ダウンロードリンクをクリック
