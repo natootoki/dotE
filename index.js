ee309dd4926dae9b384bfb696c2a8280a4069f6f
natootoki <ikoan0226@gmail.com>
2023-09-29 21:49:48 +0900

    error fix and download success

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ console.log("Hello, javaScript!")
 
 document.addEventListener("DOMContentLoaded", function () {
     var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
     if (canvas) {
-        var dataURL = canvas.toDataURL();
+
+        // 背景色を設定
+        context.fillStyle = "lightgray"; // 背景色を設定
+        context.fillRect(0, 0, canvas.width, canvas.height); // Canvas全体に描画
+
         // ここで dataURL を使った処理を行う
         var dataURL = canvas.toDataURL(); // Canvasの内容を画像データURLに変換
 
         var image = new Image();
         image.src = dataURL;
+        image.id = "imageDownload";
         document.body.appendChild(image); // 画像を表示
+        document.querySelector('#imageDownload').style.display = 'none';
 
         var link = document.createElement('a');
         link.href = dataURL;
         link.download = 'canvas_image.png'; // 画像のダウンロード時のファイル名
-        link.click(); // ダウンロードリンクをクリック
+        link.textContent = "画像をダウンロード"
+        document.body.appendChild(link); // 画像を表示
+        // link.click(); // ダウンロードリンクをクリック
     }
 });
