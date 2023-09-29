6bbd430751a05547e8c70c8deb908c264797f7ce
natootoki <ikoan0226@gmail.com>
2023-09-29 22:45:38 +0900

    random color download

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
         context.fillStyle = "lightgray"; // 背景色を設定
         context.fillRect(0, 0, canvas.width, canvas.height); // Canvas全体に描画
 
-        // ここで dataURL を使った処理を行う
         var dataURL = canvas.toDataURL(); // Canvasの内容を画像データURLに変換
 
         var image = new Image();
@@ -24,5 +23,30 @@ document.addEventListener("DOMContentLoaded", function () {
         link.textContent = "画像をダウンロード"
         document.body.appendChild(link); // 画像を表示
         // link.click(); // ダウンロードリンクをクリック
+
+        // Canvas要素にクリックイベントリスナーを追加
+        canvas.addEventListener('click', changeColor);
+
+        function changeColor() {
+            // ランダムな色を生成
+            currentColor = getRandomColor();
+            
+            // 色を変更して再描画
+            context.fillStyle = currentColor;
+            context.fillRect(0, 0, canvas.width, canvas.height);
+
+            dataURL = canvas.toDataURL(); // Canvasの内容を画像データURLに変換
+            // image.src = dataURL;
+            link.href = dataURL;
+        }
+
+        function getRandomColor() {
+            const letters = '0123456789ABCDEF';
+            let color = '#';
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
     }
 });
